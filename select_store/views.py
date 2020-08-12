d4a917f2fdafb37ad32cc80b72dcbb1423d0db6c
philiph80 <phugh80@hotmail.com>
2020-08-12 19:19:43 +0000

    Adding googlemaps test code to select store view. Work in progress

--- select_store/views.py
+++ select_store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+import googlemaps
 
-# Create your views here.
+gmaps = googlemaps.Client(key='AIzaSyA7Y1Jc7Syxl2pf04Y9wIy-HbV26wVkpzo')
 
 
 def select_store(request):
@@ -9,5 +10,12 @@ def select_store(request):
         print("request data", request.POST)
     else:
         print('not form request')
-            
+
+    origin = (43.012486, -83.6964149)
+    destination = (40.714224, -73.961452)
+
+    distance_response = gmaps.distance_matrix(origin, destination)
+    distance = int(distance_response['rows'][0]['elements'][0]['distance']['value'])
+    print('Distance response: ', distance_response, 'Distance: ', distance - 1403)
+
     return render(request, 'select_store/select-store.html')
