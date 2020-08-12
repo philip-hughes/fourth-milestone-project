0ea78d8e772adeed63d3ba2e0520546b7ed8102a
philiph80 <phugh80@hotmail.com>
2020-08-12 20:03:02 +0000

    Adding Store model

--- select_store/models.py
+++ select_store/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Store(models.Model):
+    store_name = models.CharField(max_length=20, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=False, blank=False)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    county = models.CharField(max_length=80, null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    latitude = models.FloatField(null=False, blank=False)
+    longitude = models.FloatField(null=False, blank=False)
+
+    def __str__(self):
+        return self.name
