133af91a04df65c6b682cc924c684f5cf9610ed5
philiph80 <phugh80@hotmail.com>
2020-08-15 02:03:39 +0000

    Adding initial models, urls, view and template for the menu app

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Pizza(models.Model):
+
+    name = models.CharField(max_length=30)
+    toppings = models.ManyToManyField('Topping', through='ToppingAmount', related_name='pizzas')
+
+    def __str__(self):
+        return self.name
+
+
+class Topping(models.Model):
+
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class ToppingAmount(models.Model):
+
+    REGULAR = 1
+    DOUBLE = 2
+    TRIPLE = 3
+    AMOUNT_CHOICES = (
+        (REGULAR, 'Regular'),
+        (DOUBLE, 'Double'),
+        (TRIPLE, 'Triple'),
+    )
+
+    pizza = models.ForeignKey('Pizza', related_name='topping_amounts', on_delete=models.SET_NULL, null=True)
+    topping = models.ForeignKey('Topping', related_name='topping_amounts', on_delete=models.SET_NULL, null=True, blank=True)
+    amount = models.IntegerField(choices=AMOUNT_CHOICES, default=REGULAR)    
+ 
