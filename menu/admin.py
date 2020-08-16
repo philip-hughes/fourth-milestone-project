5d78445141683ed9ae4eeeee432ffbebfa00441d
philiph80 <phugh80@hotmail.com>
2020-08-16 20:33:21 +0000

    Updating menu view and template with initial code to display pizzas and their respective toppings. Work in progress

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Pizza, Topping, ToppingAmount
+
 
 # Register your models here.
+admin.site.register(Pizza)
+admin.site.register(Topping)
+admin.site.register(ToppingAmount)
--- menu/views.py
+++ menu/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
-
+from .models import Pizza, Topping, ToppingAmount
 
 def menu(request):
-    return render()
+    pizza_list = []
+    for pizza in Pizza.objects.all():
+        pizza_item = [pizza]
+        pizza_toppings = []
+        print('Pizza: ', pizza.name)
+        for top_amt in pizza.topping_amounts.all():
+            pizza_toppings.append((top_amt.topping.name, top_amt.get_amount_display()))
+        pizza_item.append(pizza_toppings)    
+        pizza_list.append(pizza_item)
+    context = {
+        'pizzas': pizza_list
+    }       
+    return render(request, 'menu/menu.html', context)
