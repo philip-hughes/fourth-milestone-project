2cb7263cbc705a8c853ecc91e29f5eedd209f711
philiph80 <phugh80@hotmail.com>
2020-08-28 02:27:42 +0000

    Setting up basic stripe functionality, including default stripe css and js

--- checkout/views.py
+++ checkout/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def checkout(request):
-    return render(request, 'checkout/checkout.html')
+    context = {
+        'stripe_public_key': 'pk_test_51HKvzZBCwxzuCpWqiPY8CFQSS7AGDCyu80hHnvzJDF9yFnCxsZdBMDJLeGRrDDME5ShnbuW5CnoPx9547njRbCl300fLtf06W6',
+        'client_secret': 'client secret test',
+    }
+    return render(request, 'checkout/checkout.html', context)
