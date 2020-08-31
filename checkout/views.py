3ca4624c31ff844c7aa33b14cbd28f142b21a957
philiph80 <phugh80@hotmail.com>
2020-08-31 20:15:51 +0000

    Passing the order form to the checkout template

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import (render, redirect, reverse)
 from cart.contexts import cart_contents
 from django.conf import settings
 from .forms import OrderForm
@@ -8,20 +8,47 @@ import stripe
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
-    context_bag = cart_contents(request)
-    stripe_total = round(context_bag['total'] * 100)
 
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount = stripe_total,
-        currency = settings.STRIPE_CURRENCY
-    )
+    if request.method == 'POST':
+        print('THis is a POST request')
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+            'postcode': request.POST['postcode'],
+            }
 
-    context = {
-        'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
-    }
-    return render(request, 'checkout/checkout.html', context)
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.save()
+            return redirect(reverse('checkout_success'))
+
+        else:
+            print('invalid form')
+
+    else:
+        print('THis is NOT a POST request')
+        context_bag = cart_contents(request)
+        stripe_total = round(context_bag['total'] * 100)
+
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY
+        )
+        order_form = OrderForm()
+        context = {
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
+            'order_form': order_form,
+        }
+        return render(request, 'checkout/checkout.html', context)
 
 
 def checkout_success(request):
