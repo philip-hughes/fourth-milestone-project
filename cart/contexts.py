f9dab9949197fffd7ad51da5b442013c538c102e
philiph80 <phugh80@hotmail.com>
2020-09-06 23:49:46 +0000

    Adding selected store to the context, and displaying store name in the header

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,11 +1,14 @@
 from django.shortcuts import get_object_or_404
 from menu.models import Pizza
+from select_store.models import Store
 
 
 def cart_contents(request):
     cart_items = []
     total = 0
     cart = request.session.get('cart', [])
+    store_id = request.session['store']
+    store = get_object_or_404(Store, pk=store_id)
     print('Context cart: ', cart)
 
     for item in cart:
@@ -17,7 +20,8 @@ def cart_contents(request):
 
     context = {
         'cart_items': cart_items,
-        'total': total
+        'total': total,
+        'store': store
         }
     print('context:', context)
     return context
