9c95631d67188a02aad24c4df3df5b183a571424
philiph80 <phugh80@hotmail.com>
2020-09-11 01:18:14 +0000

    Adding image_url field to the product model

--- menu/models.py
+++ menu/models.py
@@ -17,6 +17,7 @@ class Product(models.Model):
     small_price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     regular_price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     large_price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
 
     def __str__(self):
         return self.name
