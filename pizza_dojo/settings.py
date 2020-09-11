20a853be1f9dacd1cbd9b41c1205af578242cbc3
philiph80 <phugh80@hotmail.com>
2020-09-11 23:01:57 +0000

    Adding media folder, and updating settings and menu template to display product images

--- pizza_dojo/settings.py
+++ pizza_dojo/settings.py
@@ -152,6 +152,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
