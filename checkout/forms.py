7dfb3228a738211c0808c147c5b556f6fe1e5ba7
philiph80 <phugh80@hotmail.com>
2020-09-15 23:29:51 +0000

    Renaming order model and form fields, and making store a required order field

--- checkout/forms.py
+++ checkout/forms.py
@@ -5,14 +5,14 @@ from .models import Order
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ('full_name', 'email', 'phone_number',
+        fields = ('name', 'email', 'phone_number',
                   'street_address1', 'street_address2',
                   'county',)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         placeholders = {
-            'full_name': 'Full Name',
+            'name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
             'street_address1': 'Street Address 1',
