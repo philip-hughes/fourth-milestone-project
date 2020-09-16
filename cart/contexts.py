3a24f0394eff0ff7f7276df5671b4c645d5c7dd0
philiph80 <phugh80@hotmail.com>
2020-09-16 01:00:18 +0000

    Adding customer address to the context

--- cart/contexts.py
+++ cart/contexts.py
@@ -5,7 +5,13 @@ from select_store.models import Store
 
 def cart_contents(request):
     store_id = request.session.get('store', '1')
-
+    customer_address = request.session['customer_address'].split(',')
+    customer_address = {
+        "street_address1" : customer_address[0],
+        "street_address2" : customer_address[1],
+        "county" : customer_address[2],
+    }
+    print('context address: ', customer_address)
     store = get_object_or_404(Store, pk=store_id)
     cart_items = []
     total = 0
@@ -23,7 +29,8 @@ def cart_contents(request):
     context = {
         'cart_items': cart_items,
         'total': total,
-        'store': store
+        'store': store,
+        'customer_address': customer_address
         }
     print('context:', context)
     return context
