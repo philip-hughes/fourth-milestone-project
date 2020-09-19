5c6af276ff96bd377212bcc5221a4fae6d54fc2c
philiph80 <phugh80@hotmail.com>
2020-09-19 02:49:38 +0000

    Adding lat/long to user profile model, and signup form processing

--- pizza_dojo/forms.py
+++ pizza_dojo/forms.py
@@ -9,17 +9,23 @@ class CustomSignupForm(SignupForm):
         self.auto_id = '%s'
     first_name = forms.CharField(max_length=30, label='First Name')
     contact_number = forms.CharField(max_length=20, label='Contact number')
-    search_input = forms.CharField(max_length=200, label='Search input', )
+    search_input = forms.CharField(max_length=200, label='Search input' )
+    loc_lat = forms.CharField(widget=forms.HiddenInput())
+    loc_long = forms.CharField(widget=forms.HiddenInput())
 
     def save(self, request):
         user = super(CustomSignupForm, self).save(request)
         contact_number = self.cleaned_data['contact_number']
         customer_address = self.cleaned_data['search_input'].split(',')
+        latitude = self.cleaned_data['loc_lat']
+        longitude = self.cleaned_data['loc_long']
         print('signup address: ', customer_address)
         user_profile = UserProfile.objects.create(user=user,
                                                   contact_number=contact_number,
                                                   street_address1=customer_address[0],
                                                   street_address2=customer_address[1],
-                                                  county=customer_address[2])
+                                                  county=customer_address[2],
+                                                  latitude=latitude,
+                                                  longitude=longitude)
         user_profile.save()
         return user
--- profiles/models.py
+++ profiles/models.py
@@ -8,6 +8,8 @@ class UserProfile(models.Model):
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
     contact_number = models.CharField(max_length=20,null=True, blank=True)
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
 
     def __str__(self):
         return self.user.username
