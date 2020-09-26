cadbdffb3a3bb7d7443bcfb3e348f60a0fb62ddc
philiph80 <phugh80@hotmail.com>
2020-09-26 02:08:09 +0000

    Updating pizza section html to match the other sections. Also adding styling for pizza item submit button

--- menu/views.py
+++ menu/views.py
@@ -15,16 +15,27 @@ def menu(request):
         menu_item = {
             'product': None,
             'sizes': [],
+
         }
         if product.product_type == 'PIZZA':
-            pizza_item = [product]
+            sizes = []
+            if product.small_price:
+                sizes.append({'price': str(product.small_price),
+                              'size': 'Small'})
+            if product.regular_price:
+                sizes.append({'price': str(product.regular_price),
+                              'size': 'Regular'})
+            if product.large_price:
+                sizes.append({'price': str(product.large_price),
+                              'size': 'Large'})
+            menu_item['product'] = product
+            menu_item['sizes'] = sizes
             pizza_toppings = []
-            print('Pizza: ', product.sku)
             for top_amt in product.topping_amounts.all():
                 pizza_toppings.append(
                     (top_amt.topping.name, top_amt.get_amount_display()))
-            pizza_item.append(pizza_toppings)
-            pizza_list.append(pizza_item)
+            menu_item['toppings'] = pizza_toppings
+            pizza_list.append(menu_item)
 
         if product.product_type == 'SIDE':
             sizes = []
