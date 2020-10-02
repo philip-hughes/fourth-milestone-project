c0575616f8625d395226b0faa96e874d550371a5
philiph80 <phugh80@hotmail.com>
2020-10-02 02:31:40 +0000

    Adding check before signup submission to verify that user has selected a valid address and show error message if not.  Also adding a line to enable the search input before submission

--- static/js/search_address.js
+++ static/js/search_address.js
@@ -1,34 +1,47 @@
-var searchInput = 'search_input';
+var searchInput = "search_input";
 
 $(document).ready(function () {
-    var autocomplete;
-    autocomplete = new google.maps.places.Autocomplete((document.getElementById(searchInput)), {
-        types: ['geocode'],
-        componentRestrictions: {
-        country: "IRL"
+  var autocomplete;
+  autocomplete = new google.maps.places.Autocomplete(
+    document.getElementById(searchInput),
+    {
+      types: ["geocode"],
+      componentRestrictions: {
+        country: "IRL",
+      },
     }
-    });
-	
-    google.maps.event.addListener(autocomplete, 'place_changed', function (e) {
-            console.log("setting lat long")
-            var near_place = autocomplete.getPlace();
-            try {
-                document.getElementById('loc_lat').value = near_place.geometry.location.lat();
-                document.getElementById('loc_long').value = near_place.geometry.location.lng();
-                $('#change-address-button').removeAttr('hidden')
-                $('#search_input').prop('disabled', true)
-                
-            }
-            catch {
-                console.log('Invalid addresss')
-            }
-    });
-    $('#change-address-button').click(function(){
-                $('#loc_lat').val("")
-                $('#loc_long').val("")
-                $('#change-address-button').prop('hidden', true)
-                $('#search_input').prop('disabled', false).val("")
-    });
+  );
 
+  google.maps.event.addListener(autocomplete, "place_changed", function () {
+    console.log("setting lat long");
+    var near_place = autocomplete.getPlace();
+    try {
+      document.getElementById('loc_lat').value = near_place.geometry.location.lat();
+      document.getElementById('loc_long').value = near_place.geometry.location.lng();
+      $("#address-error").prop("hidden", true);
+      $("#change-address-button").removeAttr("hidden");
+      $("#search_input").prop("disabled", true);
+    } catch {
+      console.log("Invalid addresss");
+    }
+  });
+  $("#change-address-button").click(function () {
+    $("#loc_lat").val("");
+    $("#loc_long").val("");
+    $("#change-address-button").prop("hidden", true);
+    $("#search_input").prop("disabled", false).val("");
+  });
+
+  $("#signup-submit").click(function (e) {
+    $("#search_input").prop("disabled", false);
+    e.preventDefault();
+    if ($("#loc_lat").val() != ""){ 
+        console.log("sumbit form")
+        $("#signup_form").submit();
+    }
+    else {
+    $("#address-error").prop("hidden", false);
+
+    }
+  });
 });
-
