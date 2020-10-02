e652c1c838eb1b107ec1eac1e3cc7b331f1153ad
philiph80 <phugh80@hotmail.com>
2020-10-02 03:42:52 +0000

    Adding code to change the scroll type to auto when an item is added to the cart, so that the screen position is maintained without the appearance of scrolling. Scroll type is then reverted to auto after the item is submitted and position is reset

--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,9 @@
         if (localStorage) {
             var posReader = localStorage["posStorage"];
             if (posReader) {
+                $('html').css('scroll-behavior', 'auto')
                 $(window).scrollTop(posReader);
+                $('html').css('scroll-behavior', 'smooth')
                 localStorage.removeItem("posStorage");
             }
             $(this).click(function(e) {
