cdad7c98ed2d80c1ea731dfcfcd343f4fcbc86f9
philiph80 <phugh80@hotmail.com>
2020-10-07 02:33:09 +0000

    Styling dropdown and adding toggle function to the dropdown button that will also toggle the 'visible' class which controls the z-index of the menu link section

--- static/js/app.js
+++ static/js/app.js
@@ -1,35 +1,47 @@
-;(function($){
-    $.fn.scrollPosReaload = function(){
-        if (localStorage) {
-            var posReader = localStorage["posStorage"];
-            if (posReader) {
-                $('html').css('scroll-behavior', 'auto')
-                $(window).scrollTop(posReader);
-                $('html').css('scroll-behavior', 'smooth')
-                localStorage.removeItem("posStorage");
-            }
-            $(this).click(function(e) {
-                localStorage["posStorage"] = $(window).scrollTop();
-            });
-            return true;
-        }
-        return false;
+(function ($) {
+  $.fn.scrollPosReaload = function () {
+    if (localStorage) {
+      var posReader = localStorage["posStorage"];
+      if (posReader) {
+        $("html").css("scroll-behavior", "auto");
+        $(window).scrollTop(posReader);
+        $("html").css("scroll-behavior", "smooth");
+        localStorage.removeItem("posStorage");
+      }
+      $(this).click(function (e) {
+        localStorage["posStorage"] = $(window).scrollTop();
+      });
+      return true;
     }
-    $(document).ready(function() {
-        console.log('document ready')
-        if ( $( "#bottom-header" ).length ) {
-                console.log('applying content style')
-                $(".content-wrapper").css('padding-top', '153px')
-                $(".content-wrapper, #select-store-wrapper").css('min-height', 'calc(100vh - 150px)')
-                /*150px = footer height*/
-            }
-        else {
-                $(".content-wrapper").css('padding-top', '103px')
-                $(".content-wrapper, #select-store-wrapper").css('min-height', 'calc(100vh - 150px)')
-                /*150px = footer height*/
-        }   
+    return false;
+  };
+  $(document).ready(function () {
+    console.log("document ready");
+    if ($("#bottom-header").length) {
+      console.log("applying content style");
+      $(".content-wrapper").css("padding-top", "153px");
+      $(".content-wrapper, #select-store-wrapper").css(
+        "min-height",
+        "calc(100vh - 150px)"
+      );
+      /*150px = footer height*/
+    } else {
+      $(".content-wrapper").css("padding-top", "103px");
+      $(".content-wrapper, #select-store-wrapper").css(
+        "min-height",
+        "calc(100vh - 150px)"
+      );
+      /*150px = footer height*/
+    }
 
-        $('.item-submit').scrollPosReaload();
-        
+    $(".item-submit").scrollPosReaload();
+
+    $("#dropdown-button").click(function () {
+         $("#section-link-container").toggleClass('visible')
+      $("#dropdown-menu").toggle("fast", function () {
+      }
+      );
     });
-}(jQuery));  
+    
+  });
+})(jQuery);
